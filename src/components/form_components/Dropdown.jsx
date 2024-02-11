9058892b3c15afcc51f0dc8eac2d392d8f6d4695
Kapilmahar987 <114247431+Kapilmahar987@users.noreply.github.com>
2024-02-11 20:42:01 +0530

    Update Dropdown.jsx

--- src/components/form_components/Dropdown.jsx
+++ src/components/form_components/Dropdown.jsx
@@ -54,60 +54,60 @@
 // export default Dropdown;
 
 
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// const Dropdown = ({ selected, setSelected, options, color }) => {
-//   const [isActive, setIsActive] = useState(false);
+const Dropdown = ({ selected, setSelected, options, color }) => {
+  const [isActive, setIsActive] = useState(false);
 
-//   const handleItemClick = (option) => {
-//     setSelected(option);
-//     setIsActive(false);
-//   };
+  const handleItemClick = (option) => {
+    setSelected(option);
+    setIsActive(false);
+  };
 
-//   return (
-//     <>
-//       <div className="form-dropdown">
-//         <div
-//           className={
-//             isActive
-//               ? "active-border form-dropdown-btn"
-//               : "normal-border form-dropdown-btn"
-//           }
-//           style={
-//             isActive
-//               ? {
-//                   color: color,
-//                   border: "1.5px solid var(--skin-color)",
-//                   boxShadow: "0 0 30px rgba(16, 15, 27, 0.15)",
-//                 }
-//               : {
-//                   color: color,
-//                   border: "1.5px solid var(--bg-black-25)",
-//                   boxShadow: "none",
-//                 }
-//           }
-//           onClick={() => setIsActive(!isActive)}
-//         >
-//           {selected}
-//           <span className="fas fa-caret-down"></span>
-//         </div>
-//         {isActive && (
-//           <div className="form-dropdown-content">
-//             {options.map((option, index) => (
-//               <div
-//                 key={index}
-//                 className="form-dropdown-item"
-//                 onClick={() => handleItemClick(option)}
-//               >
-//                 {option}
-//               </div>
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <div className="form-dropdown">
+        <div
+          className={
+            isActive
+              ? "active-border form-dropdown-btn"
+              : "normal-border form-dropdown-btn"
+          }
+          style={
+            isActive
+              ? {
+                  color: color,
+                  border: "1.5px solid var(--skin-color)",
+                  boxShadow: "0 0 30px rgba(16, 15, 27, 0.15)",
+                }
+              : {
+                  color: color,
+                  border: "1.5px solid var(--bg-black-25)",
+                  boxShadow: "none",
+                }
+          }
+          onClick={() => setIsActive(!isActive)}
+        >
+          {selected}
+          <span className="fas fa-caret-down"></span>
+        </div>
+        {isActive && (
+          <div className="form-dropdown-content">
+            {options.map((option, index) => (
+              <div
+                key={index}
+                className="form-dropdown-item"
+                onClick={() => handleItemClick(option)}
+              >
+                {option}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
-// export default Dropdown;
+export default Dropdown;
 
