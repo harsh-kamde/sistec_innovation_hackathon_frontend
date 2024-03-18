03f53b170b6ea7e31fdf71cf8583a278a49f4731
Kapilmahar987 <114247431+Kapilmahar987@users.noreply.github.com>
2024-03-18 16:19:10 +0530

    Update CurrentYearWinners.jsx

---
+++ src/components/result_components/CurrentYearWinners.jsx
@@ -0,0 +1,72 @@
+// import React, { useState } from "react";
+// // import { previousYearWinnersAPI } from "../../APIs/resultAPI";
+// import "../../stylesheets/ResultPageStyle.css";
+// import { Form, InputGroup } from "react-bootstrap";
+
+// const CurrentYearWinners = () => {
+//   const [data] = useState(CurrentYearWinnersAPI);
+
+//   // For Searching
+//   const [search, setSearch] = useState("");
+//   return (
+//     <>
+//       <section className="container result margin-top-ultra-max">
+//         <Form>
+//           <InputGroup className="my-3 search-bar">
+//             <Form.Control
+//               onChange={(e) => setSearch(e.target.value)}
+//               placeholder="Search"
+//               className="search-input"
+//             />
+//           </InputGroup>
+//         </Form>
+
+//         <div className="table-responsive">
+//           <table className="table">
+//             <thead>
+//               <tr>
+//                 <th scope="col">S. No.</th>
+//                 <th scope="col">PSID</th>
+//                 {/* <th scope="col">Team Name</th> */}
+//                 <th scope="col">Team Leader Name</th>
+//                 <th scope="col">Organization</th>
+//                 <th scope="col">PS Category</th>
+//                 <th scope="col">Problem Statement</th>
+//                 <th scope="col">Collage Name</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {data
+//                 .filter((item) => {
+//                   return search.toLowerCase() === ""
+//                     ? item
+//                     : item.PSID.toLowerCase().includes(search) ||
+//                         item.teamName.toLowerCase().includes(search) ||
+//                         item.teamLeaderName.toLowerCase().includes(search) ||
+//                         item.collage.toLowerCase().includes(search);
+//                 })
+//                 .map((element) => {
+//                   return (
+//                     <>
+//                       <tr key={element.id}>
+//                         <th scope="row">{element.id}</th>
+//                         <td>{element.PSID}</td>
+//                         {/* <td>{element.teamName}</td> */}
+//                         <td>{element.teamLeaderName}</td>
+//                         <td>{element.organization}</td>
+//                         <td>{element.PSCategory}</td>
+//                         <td>{element.problemStatement}</td>
+//                         <td>{element.collage}</td>
+//                       </tr>
+//                     </>
+//                   );
+//                 })}
+//             </tbody>
+//           </table>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default CurrentYearWinners;
