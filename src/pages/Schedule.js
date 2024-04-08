082ca8ed45b4dfabd8b305c6e28bd90570efa599
Hariom Singh Rajput <75768277+HARIOM317@users.noreply.github.com>
2024-04-08 14:58:48 +0530

    Removed Coming soon animation from schedule

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "../stylesheets/HomePageStyle.css";
 import Navbar from "../components/other/Navbar";
-import Lottie from "lottie-react";
-import ComingSoonResult from "../animations/coming_soon.json";
 import EventSchedule from "../components/home_components/EventSchedule";
 import Footer from "../components/other/Footer";
 
@@ -10,13 +8,6 @@ const Schedule = () => {
   return (
     <>
       <Navbar />
-      <section className="coming-soon">
-        <Lottie
-          loop={true}
-          animationData={ComingSoonResult}
-          className="lottie-animation"
-        />
-      </section>
       <div className="margin-top-ultra-max"></div>
       <EventSchedule />
       <Footer />
