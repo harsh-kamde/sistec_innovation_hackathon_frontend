77a35e38151f9f1424bfe43543e7a7a51f91cbc4
Hariom Singh Rajput <75768277+HARIOM317@users.noreply.github.com>
2024-04-08 14:51:51 +0530

    Updated Event Schedule of SIH 2.0 API

--- src/APIs/eventScheduleAPI.js
+++ src/APIs/eventScheduleAPI.js
@@ -3,189 +3,85 @@
 const firstDayMorningSchedule = [
   {
     id: 1,
-    time: "7:30 AM-8:00 AM",
-    task: "BreakFast",
+    time: "10:00 AM - 11:30 AM",
+    task: "Inauguration",
     description:
-      "Energize with a hearty breakfast to fuel creativity and innovation for the challenges that lie ahead.",
+      "Inauguration of Hackathon by guests.",
   },
   {
     id: 2,
-    time: "8:00 AM",
-    task: "Assemble at the venue",
+    time: "11:30 AM",
+    task: "First Round of SIH 2.0",
     description:
-      "Unite as teams gather at the venue, creating a vibrant atmosphere charged with excitement and anticipation.",
-  },
-  {
-    id: 3,
-    time: "8:15 AM-8:30 AM",
-    task: "Inauguration of the event",
-    description:
-      "Launch the event with a spirited inauguration ceremony, setting the stage for a day filled with enthusiasm and collaboration.",
-  },
-  {
-    id: 4,
-    time: "8:30 AM-8:40 AM",
-    task: "Welcome Address",
-    description:
-      "Extend warm greetings to participants, creating an inclusive and inviting atmosphere for the commencement of activities.",
-  },
-  {
-    id: 5,
-    time: "8:40AM-8:50 AM",
-    task: "Felicitation of Dignitaries",
-    description:
-      "Honour our distinguished guests, recognizing their contribution and presence as a pivotal part of the event's success.",
-  },
-  {
-    id: 6,
-    time: "8:50AM-9:00AM",
-    task: "Speech by Guest of Honor",
-    description:
-      "Gain insights from esteemed guests, drawing inspiration from their words and experiences to ignite the participants' innovative spirit.",
-  },
-  {
-    id: 7,
-    time: "9:00AM-9:10AM",
-    task: "Speech by Chief Guest at Venue",
-    description:
-      "Receive a special address from the chief guest, providing valuable perspectives and encouragement to the participants.",
-  },
-  {
-    id: 8,
-    time: "9:30 AM",
-    task: "Coding Begins",
-    description:
-      "Dive into the coding challenge, initiating a day-long journey of problem-solving and creativity.",
-  },
-  {
-    id: 9,
-    time: "9:55 AM",
-    task: "Tea and snacks on table",
-    description:
-      "Take a refreshing break with tea and snacks, fostering networking and rejuvenating the participants for the tasks ahead.",
-  },
-  {
-    id: 10,
-    time: "10:00AM -11:00AM",
-    task: "First Round of Training Session",
-    description:
-      "Engage in the first round of training sessions, equipping participants with essential skills and knowledge to excel in the challenges.",
-  },
+      "Start First Round Judgement by various Judges. You can start making corrections/Updation.",
+  }
 ];
 
 const firstDayAfterMorningSchedule = [
   {
     id: 1,
-    time: "12:00 -12:30 PM",
+    time: "01:00 PM",
     task: "Lunch",
     description:
       "Refuel during lunch, fostering camaraderie and providing an opportunity for participants to recharge for the upcoming coding tasks.",
   },
   {
     id: 2,
-    time: "12:30 PM",
-    task: "Coding Resumes",
+    time: "05:00 PM",
+    task: "Tea Break",
     description:
-      "Return to coding tasks, where participants apply newfound skills and collaborate on innovative solutions.",
+      "Recharge with tea, providing a boost of energy and creating a dynamic environment for the continuation of coding.",
   },
   {
     id: 3,
-    time: "3:00 PM",
-    task: "Snacks on Table",
+    time: "05:30 PM",
+    task: "Second Round of Judgement",
     description:
-      " Recharge with snacks, providing a boost of energy and creating a dynamic environment for the continuation of coding.",
+      "Start the Second Round of Judgement based on Suggestion correction given by the judges in the first round.",
   },
   {
     id: 4,
-    time: "5:00 PM",
-    task: "First Assessment",
-    description:
-      "Evaluate progress with the first assessment, offering valuable insights and feedback to enhance participants' coding skills.",
-  },
-  {
-    id: 5,
-    time: "8:00PM-8:30PM",
+    time: "8:00 PM",
     task: "Dinner",
     description:
       "Conclude the day with a hearty evening meal, providing a relaxed setting for participants to unwind and reflect on their achievements.",
-  },
-  {
-    id: 6,
-    time: "8:30PM",
-    task: "Coding Resumes",
-    description:
-      "Resume coding after dinner, encouraging participants to explore new ideas and refine their projects.",
-  },
-  {
-    id: 7,
-    time: "11:00PM",
-    task: "Second Assessment",
-    description:
-      "Conduct the second assessment, allowing participants to showcase advancements made throughout the day and receive constructive feedback.",
-  },
+  }
 ];
 
 const secondDayMidNightSchedule = [
   {
     id: 1,
-    time: "12:30AM",
-    task: "Second Round of Training (5 to 7 mins per team)",
+    time: "03:30 AM",
+    task: "Third Round of Judgement",
     description:
-      "Engage in a brief night training session, offering participants additional insights and strategies for tackling challenges.",
-  },
-  {
-    id: 2,
-    time: "12:45AM",
-    task: "Snacks on Table",
-    description:
-      "Indulge in midnight snacks, providing sustenance and fostering a collaborative spirit during the late-night coding session.",
-  },
-  {
-    id: 3,
-    time: "12:50AM",
-    task: "Coding Continues",
-    description:
-      "Continue coding into the night, allowing participants to delve deeper into their projects and refine their solutions.",
-  },
-  {
-    id: 4,
-    time: "3:00AM",
-    task: "Snacks and biscuits served on Table",
-    description:
-      "Kickstart the morning with early snacks, offering a nutritious start to the day and sustaining participants during the final coding stretch.",
+      "Third Round of Judgement based on Suggestion correction given by the judges in first round and second round.",
   },
 ];
 
 const secondDayMorningSchedule = [
   {
     id: 1,
-    time: "6:00AM- 6:15AM",
+    time: "05:00 AM",
     task: "Yoga Session",
     description:
       "Refresh with a morning yoga session, promoting physical and mental well-being to enhance focus and creativity.",
   },
   {
     id: 2,
-    time: "7:00AM",
+    time: "09:00 AM",
     task: "Breakfast served",
     description:
       "Begin the day with a wholesome breakfast, setting the tone for the final round of evaluations and activities.",
   },
   {
     id: 3,
-    time: "8:30AM",
-    task: "Final Round of Evaluation",
-    description:
-      "Conclude with the last round of project evaluations, providing participants with an opportunity to showcase their innovative solutions.",
-  },
-  {
-    id: 4,
-    time: "10:00AM",
+    time: "10:00 AM",
     task: "Valedictory Function and Prize distribution",
     description:
       "Celebrate the conclusion of the event with a valedictory function, recognizing outstanding contributions and distributing prizes to deserving participants.",
   },
 ];
+
 
 // ========== For Year 2023 ==========
 
